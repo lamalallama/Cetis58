ce6a2c44d3a87cc4c663b4288e8d73b59986ee8d
DaturaFerox <66500636+lamalallama@users.noreply.github.com>
2023-09-11 22:30:21 -0700

    Create UserInput.cs

---
+++ UserInput.cs
@@ -0,0 +1,12 @@
+using System;
+
+int sum = 0;
+
+Console.Write("Enter a number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter another number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+sum = num1 + num2;
+Console.WriteLine("Sum: " + sum);
